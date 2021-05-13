c53f6cb2c224ed11b0e6cfa41b6dfab9fd43a50c
Kirk Waters <kirkwaters@quickenloans.com>
2021-05-13 17:03:10 -0400

    First commit of Lab 11.2 and 11.3

---
+++ Lab11.2/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Lab11._2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace Lab11._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            //return Content("Here is the registration page");
+            return View();
+        }
+
+        public IActionResult FormSubmit(User user, string password1)
+        {
+            //return Content("Here's the form submit page");
+
+            Regex rx = new Regex(@"[0-9]+");
+
+            if(rx.IsMatch(user.FirstName) || rx.IsMatch(user.LastName))
+            {
+                return Content("Your name cannot contain any numbers.");
+            }
+
+            // need to add more validation here. 
+            if(user.Password != password1)
+            {
+                return Content("Sorry, the passwords do not match");
+            }
+
+            Models.User.currentUser = user;
+
+            return View(user);
+        }
+
+        public IActionResult ViewProfile(User user)
+        {
+            return View(Models.User.currentUser);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab11.2/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab11._2.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+        public string State { get; set; }
+
+        public static User currentUser;
+    }
+}
